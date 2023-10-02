88baa2a115dc1a7a9dfeb7b6a170f5fe20eb1c84
Shenghao Jia <sjia23@ucsc.edu>
2023-10-01 17:49:32 -0700

    Two more problem solutions

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -6,3 +6,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        if nums is None:
+            return 0
+        j = 0
+        for i in range(len(nums)):
+
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+        return nums
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [0, 1, 0, 3, 12]
+    ans = s.moveZeroes(nums)
+    print(ans)
